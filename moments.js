fb4c142a769b3f8b19d8c764ae4f4da910af6a18
Coding Club <teamcdcofc@gmail.com>
2025-12-04 17:03:51 +0530

    Create moments.js

---
+++ moments.js
@@ -0,0 +1,140 @@
+const momentsData = {
+  "moments": [
+    {
+      "id": "Table Tennis",
+      "title": "Table Tennis",
+      "description": "A fast-paced table tennis rally ends with a sharp smash and a thrilling match point for victory!",
+      "house": "Gemunu",
+      "album": "https://web.facebook.com/share/p/1B28FoPQim/",
+      "image": "https://github.com/bcteamcdc/SportMeet-Marks-Board-2025/blob/main/Images/Table-Tenis/486330448_1225101639619107_179522079002211673_n.jpg?raw=true"
+    },
+    {
+      "id": "Karate",
+      "title": "Karate",
+      "description": "An intense karate showdown ends with a powerful final strike and a decisive point for victory!",
+      "house": "Gemunu",
+      "album": "https://web.facebook.com/share/p/1ANWHT3jez/",
+      "image": "https://github.com/bcteamcdc/SportMeet-Marks-Board-2025/blob/main/Images/Karate/486582157_1225009662961638_4121651046955543575_n.jpg?raw=true"
+    },
+    {
+      "id": "Hardball",
+      "title": "Hardball",
+      "description": "A fast-paced hardball rally ends with a powerful shot and a thrilling match point for victory!",
+      "house": "Vijaya",
+      "album": "https://www.facebook.com/share/p/18vBj7EDmX/",
+      "image": "https://github.com/bcteamcdc/SportMeet-Marks-Board-2025/blob/main/Images/Hardball/FB_IMG_1743346529213.jpg?raw=true"
+    },
+    {
+      "id": "Wrestling",
+      "title": "Wrestling",
+      "description": "Wrestling is a combat sport where athletes use strength and technique to pin or submit their opponents",
+      "house": "Tissa",
+      "album": "https://web.facebook.com/share/p/1ADJy7tMgb/",
+      "image": "https://github.com/bcteamcdc/SportMeet-Marks-Board-2025/blob/main/Images/Wrestling/487794152_1227835969345674_5135041037800775890_n.jpg?raw=true"
+    },
+    {
+      "id": "Boxing",
+      "title": "Boxing",
+      "description": "A fierce boxing bout concludes with a flurry of punches and a stunning knockout for victory!",
+      "house": "Parakrama",
+      "album": "https://web.facebook.com/share/p/18pBoPUTfs/",
+      "image": "https://github.com/bcteamcdc/SportMeet-Marks-Board-2025/blob/main/Images/Boxing/486773171_1224080526387885_1949751398889016980_n.jpg?raw=true"
+    },
+    {
+      "id": "Football",
+      "title": "Football",
+      "description": "Football is a team sport where two teams compete to score goals by kicking a ball into the opponent's net, using skill, strategy, and teamwork.",
+      "house": "Parakrama",
+      "album": "https://web.facebook.com/share/p/191dXY7FWz/",
+      "image": "https://github.com/bcteamcdc/SportMeet-Marks-Board-2025/blob/main/Images/Football/487126037_1228219689307302_7217693971095667339_n.jpg?raw=true"
+    },
+    {
+      "id": "Athletics",
+      "title": "Athletics",
+      "description": "A fierce swim race ends with a final surge and a thrilling wall touch for victory!",
+      "house": "Gemunu",
+      "album": "https://web.facebook.com/share/p/16DwWwmtCp/",
+      "image": "https://github.com/bcteamcdc/SportMeet-Marks-Board-2025/blob/main/Images/Athletics/485131274_1219019323560672_5228274866754982966_n.jpg?raw=true"
+    },
+    {
+      "id": "Swimming",
+      "title": "Swimming Championship",
+      "description": "A fierce swim race ends with a final surge and a thrilling wall touch for victory!",
+      "house": "Gemunu",
+      "album": "https://www.facebook.com/share/p/1DMAZKabhv/",
+      "image": "https://github.com/bcteamcdc/SportMeet-Marks-Board-2025/blob/main/Images/Swimming/FB_IMG_1742521240493.jpg?raw=true"
+    },
+    {
+      "id": "Tag-Rugby-final",
+      "title": "Tag-Rugby Championship Final",
+      "description": "A fast-paced tag rugby game concludes with a lightning-quick dodge and a game-winning try!",
+      "house": "Tissa",
+      "album": "https://web.facebook.com/share/p/16AZ89LLdX/",
+      "image": "https://github.com/bcteamcdc/SportMeet-Marks-Board-2025/blob/main/Images/Tag-Rugby/481509527_1206375371491734_2598653844130955176_n.jpg?raw=true"
+    },
+    {
+      "id": "Road Race",
+      "title": "Road Race Championship",
+      "description": "Tissa house dominates the road race with a spectacular finish!",
+      "house": "Tissa",
+      "album": "https://www.facebook.com/share/p/15DcVEWEj8/",
+      "image": "https://github.com/bcteamcdc/SportMeet-Marks-Board-2025/blob/main/Images/Road-Race/FB_IMG_1741784061409.jpg?raw=true"
+    },
+    {
+      "id": "Badminton-final",
+      "title": "Badminton Championship Final",
+      "description": "A thrilling badminton match ends with a spectacular smash securing victory!",
+      "house": "Vijaya",
+      "album": "https://web.facebook.com/share/p/12DwV6oMERB/",
+      "image": "https://raw.githubusercontent.com/bcteamcdc/SportMeet-Marks-Board-2025/refs/heads/main/Images/Badminton/481947705_1201495678646370_3815352414909344404_n.jpg"
+    },
+    {
+      "id": "Chess-final",
+      "title": "Chess Championship Final",
+      "description": "A tense chess battle reaches its climax as a brilliant checkmate secures the decisive victory!",
+      "house": "Parakrama",
+      "album": "https://web.facebook.com/share/p/1BJSzFakk6/",
+      "image": "https://github.com/bcteamcdc/SportMeet-Marks-Board-2025/blob/main/Images/Chess/FB_IMG_1741564404128.jpg?raw=true"
+    },
+    {
+      "id": "Volleyball-final",
+      "title": "Volleyball Championship Final",
+      "description": "A thrilling volleyball match reaches its climax as a powerful spike seals the game-winning point!",
+      "house": "Tissa",
+      "album": "https://web.facebook.com/share/p/1BayHpvBu6/",
+      "image": "https://github.com/bcteamcdc/SportMeet-Marks-Board-2025/blob/main/Images/Volleyball/481777854_1206507628145175_230429400909542038_n.jpg?raw=true"
+    },
+    {
+      "id": "Elle-final",
+      "title": "Elle Championship Final",
+      "description": "Elle secures a stunning win in the final moments",
+      "house": "Gemunu",
+      "album": "https://web.facebook.com/share/p/15FsNmbdyk/",
+      "image": "https://github.com/bcteamcdc/SportMeet-Marks-Board-2025/blob/main/Images/Elle/481293868_1198761235586481_6186605271315011251_n.jpg?raw=true"
+    },
+    {
+      "id": "Hockey-final",
+      "title": "Hockey Championship Final",
+      "description": "Hockey team clinches victory with a last-minute goal!",
+      "house": "Tissa",
+      "album": "https://www.facebook.com/share/p/1FcPbHUT11/",
+      "image": "https://github.com/bcteamcdc/SportMeet-Marks-Board-2025/blob/main/Images/Hockey/FB_IMG_1741650974735.jpg?raw=true"
+    },
+    {
+      "id": "Softball-final",
+      "title": "Softball Championship Final",
+      "description": "A dramatic softball showdown ends with a clutch hit securing the victory!",
+      "house": "Gemunu",
+      "album": "https://web.facebook.com/share/p/15TioPZ4pL/",
+      "image": "https://raw.githubusercontent.com/bcteamcdc/SportMeet-Marks-Board-2025/refs/heads/main/Images/Softball/480703812_1201348868661051_1014252395281935854_n.jpg"
+    },
+    {
+      "id": "BasketBall-final",
+      "title": "BasketBall Champions",
+      "description": "Epic basketball showdown ends with a buzzer-beater win",
+      "house": "Tissa",
+      "album": "https://web.facebook.com/share/p/15aWoAKz4R/",
+      "image": "https://raw.githubusercontent.com/bcteamcdc/SportMeet-Marks-Board-2025/main/Images/BasketBall/480881746_1198555862273685_2223825316418136102_n.jpg"
+    }
+  ]
+};
